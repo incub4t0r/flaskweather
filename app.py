fe15558aac38bbdd9c93da65289019176adeb7cb
incub4t0r <dchung0812@gmail.com>
2022-07-27 02:26:14 +0000

    updated selector and added a new layout

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, url_for, redirect
 
+DEV = True
 app = Flask(__name__)
 
 # @app.route('/', methods=['GET','POST'])
@@ -23,10 +24,19 @@ def stand():
     cityID = request.args.get('cityID', None)
     return render_template('stand.html', cityID=cityID)
 
-# @app.route('/selector')
+@app.route('/stack')
+def stack():
+    cityID = request.args.get('cityID', None)
+    return render_template('stack.html', cityID=cityID)
 
+# @app.route('/testing')
+# def testing():
+#     return render_template('testing.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    if DEV:
+        app.run(host="127.0.0.1", port=6464, debug=True)
+    else:
+        app.run(host='0.0.0.0', debug=True)
 
 
