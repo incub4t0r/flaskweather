84cb68008762170a2e8977bc61814f13fa17d738
incub4t0r <dchung0812@gmail.com>
2023-05-31 20:51:12 -0400

    updated to allow hex color choosing and defaults to black background

--- static/js/backdrop.js
+++ static/js/backdrop.js
@@ -1,11 +1,29 @@
+// const default_backdrop = "#1f1f1f";
+const default_backdrop = "#000000";
+
 $(window).on('load', function() {
     $('html').addClass('hidden');
     if (localStorage.getItem("backdrop_url") === null) {
-        reset_backdrop();
-    } else {
+        // reset_backdrop();
+        set_backdrop_black();
+    } 
+    else {
         var backdrop = $('body');
-        backdrop.css({'background':"url(" + localStorage.getItem("backdrop_url") + ") no-repeat center center fixed", "background-size":"cover"});
+        if (localStorage.getItem("backdrop_url") === default_backdrop) {
+            backdrop.css({'background-color':default_backdrop});
+        }
+        else {
+            const backdrop_val = localStorage.getItem("backdrop_url");
+            if (backdrop_val.length == 6) {
+                backdrop.css({'background-color': `#${backdrop_val}`});
+            }
+            else {
+                backdrop.css({'background':"url(" + backdrop_val + ") no-repeat center center fixed", "background-size":"cover"});
+            }
+            // backdrop.css({'background':"url(" + localStorage.getItem("backdrop_url") + ") no-repeat center center fixed", "background-size":"cover"});
+        }
     }
+
     $(document).ready(function() {  
         $('html').removeClass('hidden');  // EDIT: Can also use $('html').removeClass('hidden'); 
     });
@@ -16,8 +34,6 @@ $(window).on('load', function() {
         }
         const reader = new FileReader();
         reader.onload = function fileReadCompleted() {
-            // document.body.style.background = "url(" + reader.result + ") no-repeat center center fixed";
-            // document.body.style.backgroundSize = "cover";
             var body = $('body');
             body.css({'background':"url(" + reader.result + ") no-repeat center center fixed", "background-size":"cover"});
             localStorage.setItem("backdrop_url", reader.result);
@@ -28,12 +44,31 @@ $(window).on('load', function() {
 
 function set_backdrop() {
     event.preventDefault();
-    var backdrop = $("#custom_backdrop").val()
+    var backdrop = $("#custom_backdrop").val();
     var body = $('body');
-    body.css({'background':"url(" + backdrop + ") no-repeat center center fixed", "background-size":"cover"});
-    console.log("url(" + backdrop + ")");
-    localStorage.setItem("backdrop_url", backdrop);
+    if (backdrop.length == 6) {
+        body.css({'background-color': `#${backdrop}`});
+        console.log("Backdrop value is a hex code, setting background-color: " + backdrop);
+        localStorage.setItem("backdrop_url", backdrop);
+    }
+    else {
+        body.css({'background':"url(" + backdrop + ") no-repeat center center fixed", "background-size":"cover"});
+        console.log("url(" + backdrop + ")");
+        localStorage.setItem("backdrop_url", backdrop);
+    }
     $("#custom_backdrop").val('');
+}
+
+function set_backdrop_black() {
+    try {
+        // swal_confirm();
+        var backdrop = $('body');
+        backdrop.css({'background-color':default_backdrop});
+        localStorage.setItem("backdrop_url",default_backdrop);
+    }
+    catch (error) {
+        console.log("error: " + error);
+    }
 }
 
 function reset_backdrop(){
@@ -41,12 +76,13 @@ function reset_backdrop(){
         localStorage.setItem("backdrop_url", "../static/img/blob-scene-haikei-sunset.svg");
         var backdrop = $('body');
         backdrop.css({'background':"url(../static/img/blob-scene-haikei-sunset.svg) no-repeat center center fixed", "background-size":"cover"});
-    } catch (error) {
+    } 
+    catch (error) {
         console.log("error: " + error);
     }
 }
 
-function swal_confirm() {
+function swal_reset_backdrop() {
     event.preventDefault();
     swal({
         title: "Are you sure?",
@@ -55,7 +91,8 @@ function swal_confirm() {
         dangerMode: true,
     }).then((willDelete) => {
         if (willDelete) {
-            reset_backdrop();
+            set_backdrop_black();
+            // reset_backdrop();
             swal("Poof! Your backdrop has been reset.", {
                 icon: "success",
             });
